81eb7d89e264bc44529d10c24f7a12a5575f365d
Andrew Valancius <avalancius6@gmail.com>
2019-07-19 16:39:31 -0400

    Add account methods

--- src/main/java/com/revature/babank/Account.java
+++ src/main/java/com/revature/babank/Account.java
@@ -1,44 +1,57 @@
 package com.revature.babank;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Account {
 	int id;
 	double balance;
-	Date dateCreated;
+	LocalDateTime dateCreated;
 	String accountType;
 	
-	//transaction history
+	// TODO transaction history
 	
 	boolean isOpen;
 	
 	Date dateClosed;
 	// lets store who closed the account
 	
+//	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+//	LocalDateTime now = LocalDateTime.now();  
+//	System.out.println(dtf.format(now));  
 	
-	
-	
-	
-	public void deposit(double dollars) {
-		
+	public Account(int id, String accountType) {
+		this(id, 0, LocalDateTime.now(), accountType, true, null);
 	}
 	
-	public Account(int id, double balance, Date dateCreated, String accountType, boolean isOpen, Date dateClosed) {
+	
+	
+	
+	public Account(int id, double balance, LocalDateTime dateCreated, String accountType, boolean isOpen, Date dateClosed) {
 		super();
 		this.id = id;
 		this.balance = balance;
+		
+		
+		
 		this.dateCreated = dateCreated;
 		this.accountType = accountType;
 		this.isOpen = isOpen;
 		this.dateClosed = dateClosed;
 	}
 
+	public void deposit(double dollars) {
+		balance += dollars;
+	}
+	
 	public void withdraw(double dollars) {
-		
+		balance = balance - dollars;
+		return;
 	}
 	
 	public void transfer(double dollars, Account acc) {
-		
+		balance = balance - dollars;
+		acc.balance = acc.balance + dollars;
 	}
 	
 }
