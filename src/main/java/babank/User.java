1f3e2440a2a66013247c93abdf6fc44f1320ffba
Andrew Valancius <avalancius6@gmail.com>
2019-07-23 00:57:29 -0400

    Add getters and setters for User

--- src/main/java/babank/User.java
+++ src/main/java/babank/User.java
@@ -21,6 +21,62 @@ public class User implements Serializable {
 		this.name = name;
 		this.type = type;
 	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public UserType getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(UserType type) {
+		this.type = type;
+	}
 	
 	
 	
